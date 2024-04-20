e985b905a61401880ee49b96846112065ca3f59e
Gladwin Ferdz Del Rosario <85918854+gfdelrosario12@users.noreply.github.com>
2024-04-20 16:08:19 +0000

    third autopilot commit

--- comment.js
+++ comment.js
@@ -1,27 +1,77 @@
-// Create web server and load data from json file
-// Return the data in JSON format
+// Create web server to handle comment request for comment using express router
 
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const express = require('express');
+const router = express.Router();
+const Comment = require('../models/comment');
+const User = require('../models/user');
 
-var server = http.createServer(function(request, response) {
-    var path = url.parse(request.url).pathname;
-    if (path == '/comment') {
-        fs.readFile(__dirname + '/comment.json', function(err, data) {
-            if (err) {
-                throw err;
-            }
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.write(data);
-            response.end();
-        });
-    } else {
-        response.writeHead(404, {'Content-Type': 'text/html'});
-        response.write('404 Not Found\n');
-        response.end();
+// Get all comments
+router.get('/', async (req, res) => {
+    try {
+        const comments = await Comment.find();
+        res.json(comments);
+    } catch (err) {
+        res.json({ message: err });
     }
 });
 
-server.listen(8000);
-console.log('Server running at http://localhost:8000/');
+// Get a specific comment
+router.get('/:commentId', async (req, res) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        res.json(comment);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// Get all comments by a specific user
+router.get('/user/:userId', async (req, res) => {
+    try {
+        const comments = await Comment.find({ userId: req.params.userId });
+        res.json(comments);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// Post a comment
+router.post('/', async (req, res) => {
+    const comment = new Comment({
+        userId: req.body.userId,
+        content: req.body.content,
+        date: req.body.date
+    });
+
+    try {
+        const savedComment = await comment.save();
+        res.json(savedComment);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// Update a comment
+router.patch('/:commentId', async (req, res) => {
+    try {
+        const updatedComment = await Comment.updateOne(
+            { _id: req.params.commentId },
+            { $set: { content: req.body.content } }
+        );
+        res.json(updatedComment);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// Delete a comment
+router.delete('/:commentId', async (req, res) => {
+    try {
+        const removedComment = await Comment.deleteOne({ _id: req.params.commentId });
+        res.json(removedComment);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+module.exports = router;
